490e95440ea837437f760a1aa92233b74b98eb41
Ariel Behar <ariel.behar@hotmail.com>
2020-05-21 15:51:57 +0300

    Updating form processing with AJAX

--- routes/routing.js
+++ routes/routing.js
@@ -55,7 +55,6 @@ router.post('/', urlencodedParser, async (req, res) => {
             console.log(error);
         } else {
             console.log('Message sent: %s', info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         }
     });
 });
